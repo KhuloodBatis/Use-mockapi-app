3a95589d955fd4ac12c53993627835d4ec228b1d
Khulood Batis <50932099+KhuloodBatis@users.noreply.github.com>
2023-08-09 20:49:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-//you must change the name methode 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
